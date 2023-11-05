c90f6f0f1cea0c005d2b0166d5b4699e15bcd328
Ian <87252958+Ianszzkieyyy@users.noreply.github.com>
2023-11-05 10:33:51 +0800

    Setup React Env

---
+++ flashcard-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flashcard App</h1>
+      <p>Upload your CSV file here</p>
+    </div>
+  );
+}
+
+export default App;
